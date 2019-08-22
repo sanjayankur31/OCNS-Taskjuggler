ba3954f822296cd7bb9e90cdbc6b8e746e6fbf34
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2019-08-22 23:03:30 +0100

    Change output folder to docs for github pages

--- make-reports.sh
+++ make-reports.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Copyright 2019 Ankur Sinha
-# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com> 
+# Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 # File : make-reports.sh
 #
 
-mkdir reports && pushd reports && tj3 ../src/CNS2020.tjp && popd
+mkdir docs
+
+pushd docs
+    tj3 ../src/CNS2020.tjp
+popd
